f9fc68a1dcd8a94636534ca329d586f8604c5b2c
Tolgahan Üzün <mail@tolgahanuzun.com>
2019-12-12 14:50:43 +0300

    Licanse and pypi file added

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+  name = 'test_plus',
+  packages = ['test_plus'], 
+  version = '0.1.0',
+  description = 'Test analysis tool for Django',
+  author = 'Tolgahan Üzün',
+  author_email = 'mail@tolgahanuzun.com',
+  url = 'https://github.com/tolgahanuzun/test_plus',
+  download_url = 'https://github.com/tolgahanuzun/test_plus/tarball/0.1.0',
+  keywords = ['django', 'test', 'time'],
+  install_requires = [],
+  classifiers = [],
+)
