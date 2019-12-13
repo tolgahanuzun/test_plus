68230d8c8131193e0c7d07d69b249f3d6681fcd6
Tolgahan Üzün <mail@tolgahanuzun.com>
2019-12-13 17:01:24 +0300

    Readme update

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
-import os
+from os import path
 from distutils.core import setup
 from setuptools import setup, find_packages
 
-BASE_DIR = os.path.dirname(__file__)
-f = open(os.path.join(BASE_DIR, "readme.md"))
-readme = f.read()
-f.close()
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'readme.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
   name = 'test_plus',
   packages = find_packages(), 
   version = '0.1.10',
-  long_description=readme,
+  long_description=long_description,
   long_description_content_type="text/markdown",
   description = 'Test analysis tool for Django',
   author = 'Tolgahan Üzün',
