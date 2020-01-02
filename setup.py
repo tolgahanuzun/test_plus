40aa733aec7aa48d9c71d7314ace4fe49b8a13f4
Tolgahan Üzün <mail@tolgahanuzun.com>
2020-01-02 09:37:42 +0300

    Typo fixed

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from os import path
 from distutils.core import setup
 from setuptools import setup, find_packages
 
-this_directory = path.abspath(path.dirname(__file__))
+this_directory = path.abspath(path.dirname('__file__'))
 with open(path.join(this_directory, 'readme.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
   name = 'test_plus',
   packages = find_packages(), 
-  version = '0.1.10',
-  long_description=long_description,
+  version = '0.1.11',
   long_description_content_type="text/markdown",
+  long_description=long_description,
   description = 'Test analysis tool for Django',
   author = 'Tolgahan Üzün',
   author_email = 'mail@tolgahanuzun.com',
